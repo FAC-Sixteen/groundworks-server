c4ba8d378d58574ba09a3adbc4fc1f48c709b90c
James Levine <jamesalexanderlevine@gmail.com>
2019-06-07 16:55:29 +0100

    add brief schema

---
+++ database/schemas/Brief.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose"); // calling mongoose
+const Schema = mongoose.Schema; //accessing Schema from Mongoose Object.
+
+const Brief = new Schema({
+  companyName: { type: String, required: true, maxlength: 50 },
+  contactPerson: { type: String, required: true, lowercase:true, maxlength: 100 },
+  projectName: { type: String, required: true, lowercase:true, maxlength: 100 },
+  projectBrief: { type: String, required: true, maxlength: 5000 },
+  projectDeadline: { type: Date, required: true },
+  estimatedWorkload: { type: Number, required: true },
+  projectPrice: { type: Number, required: true },
+  studentSkills: { type: [String], lowercase: true, required: true },
+  additionalInfo: { type: String, required: true, maxlength: 1500 },
+});
+
+module.exports = mongoose.model("Brief", Brief);
