d6d7de88beb5390fb3bcefb91f4a7a0a483cd90c
Ryan McAvoy <ryanmcavoy@protonmail.com>
2019-06-19 16:57:24 +0100

    amend schema

--- controllers/studentControllers.js
+++ controllers/studentControllers.js
@@ -1,10 +1,9 @@
-const Student = require("../database/schemas/Student");
-const Register = require("../database/schemas/Register");
+const User = require("../database/schemas/User");
 
 //get all student documents from database
 exports.getAllStudents = async (req, res) => {
   try {
-    const students = await Student.find();
+    const students = await User.find();
     res.json(students);
   } catch (err) {
     res.json({ message: err });
@@ -15,7 +14,7 @@ exports.getAllStudents = async (req, res) => {
 //IMPORTANT: this will delete the entire database.  Remove before deployment.
 exports.deleteAllStudents = async (req, res) => {
   try {
-    const students = await Student.deleteMany({ __v: 0 }, function(err) {});
+    const students = await User.deleteMany({ __v: 0 }, function(err) {});
     res.json(students);
   } catch (err) {
     res.json({ message: err });
@@ -26,17 +25,17 @@ exports.deleteAllStudents = async (req, res) => {
 exports.postStudentSignUp = async (req, res) => {
   try {
     // studentData is assigning object contain info to various variables i.e. firstName etc.
-    const studentData = new Student({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
+    const studentData = new User({
+      userName: req.body.userName,
       email: req.body.email,
-      phoneNumber: req.body.phoneNumber,
       password: req.body.password,
+      phoneNumber: req.body.phoneNumber,
+      userType: req.body.userType,
       university: req.body.university,
       yearOfStudy: req.body.yearOfStudy,
       courseStudied: req.body.courseStudied,
-      aboutYou: req.body.aboutYou,
       skills: req.body.skills,
+      aboutYou: req.body.aboutYou,
       LinkedinURL: req.body.LinkedinURL
     });
     const newStudent = await studentData.save(); // wait for studentData before saving and storing in newStudent
@@ -49,7 +48,7 @@ exports.postStudentSignUp = async (req, res) => {
 exports.student_findById = async (req, res) => {
   try {
     console.log(req.params);
-    const profile = await Student.findById(req.params.studentID);
+    const profile = await User.findById(req.params.studentID);
     res.json(profile);
   } catch (err) {
     res.json({ message: err });
@@ -58,9 +57,9 @@ exports.student_findById = async (req, res) => {
 
 exports.student_update = async (req, res) => {
   try {
-    const update = await Student.findByIdAndUpdate(
+    const update = await User.findByIdAndUpdate(
       req.params.studentID,
-      req.body.firstName
+      req.body.userName
     ); //update
     res.json(update);
   } catch (err) {
@@ -72,10 +71,11 @@ exports.student_update = async (req, res) => {
 exports.postRegisterStudent = async (req, res) => {
   console.log("req", req.body);
   try {
-    const newStudentData = new Register({
-      name: req.body.name,
+    const newStudentData = new User({
+      userName: req.body.userName,
       email: req.body.email,
-      password: req.body.password
+      password: req.body.password,
+      userType: req.body.userType
     });
     const newRegisterStudent = await newStudentData.save();
     res.json(newRegisterStudent);
@@ -86,7 +86,7 @@ exports.postRegisterStudent = async (req, res) => {
 
 exports.getAllStudentRegisters = async (req, res) => {
   try {
-    const studentsRegisters = await Register.find();
+    const studentsRegisters = await User.find();
     res.json(studentsRegisters);
   } catch (err) {
     res.json({ message: err });
